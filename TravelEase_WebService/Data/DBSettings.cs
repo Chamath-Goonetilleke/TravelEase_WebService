f25013e6688db67ea23c593e317c8ec651a59629
IT20122096 <it20122096@my.sliit.lk>
2023-09-13 15:59:34 +0530

    first commit

---
+++ TravelEase_WebService/Data/DBSettings.cs
@@ -0,0 +1,9 @@
+namespace TavelEase_WebService.Data
+{
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+        public string UserCollectionName { get; set; } = string.Empty;
+    }
+}
---
+++ TravelEase_WebService/Program.cs
@@ -0,0 +1,10 @@
+﻿using TavelEase_WebService.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+

a5b97aaca1fa89708c8b9a19d824e1cdaebe0906
IT20122096 <it20122096@my.sliit.lk>
2023-10-04 19:55:30 +0530

    IT20122096 - dev branch

--- TravelEase_WebService/Program.cs
+++ TravelEase_WebService/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World");
 
 app.Run();
 
